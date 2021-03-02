40c0d6da62131174854c874c0c6e5f27ead41974
anapgh <anapogarciah@gmail.com>
2021-03-02 13:42:30 +0100

    Ejemplo 8: lectura sincrona

--- P1/S3/Ej08-read-sync.js
+++ P1/S3/Ej08-read-sync.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 console.log("Lectura síncrona de un fichero");
 
 //-- Realizar la lectura
-const data = fs.readFileSync(' ','utf8');
+const data = fs.readFileSync('fich1.txt','utf8');
 
 //-- Esta instrucción se ejecuta una vez terminada
 //-- la lectura síncrona
