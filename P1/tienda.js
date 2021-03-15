48a6912ca61e38cbea58db75fa349b6d9c0abd36
anapgh <anapogarciah@gmail.com>
2021-03-15 20:30:12 +0100

    fallo de path corregido, 200ok

--- P1/tienda.js
+++ P1/tienda.js
@@ -9,7 +9,7 @@ const url = require('url');
 const PUERTO = 9000;
 
 //-- Mensaje de arranque
-console.log("Arrancando servidor...")
+console.log("Arrancando servidor...");
 
 //-- Crear el sevidor
 const server = http.createServer(function (req, res) {
@@ -22,29 +22,29 @@ const server = http.createServer(function (req, res) {
   let myURL = url.parse(req.url, true);
 
   //-- Escribimos en consola la ruta de nuestro recurso
-  console.log("Recurso recibido: " + myURL.pathname)
+  console.log("Recurso recibido: " + myURL.pathname);
 
   //-- Definimos una variable fichero
-  let filename = ""
+  let filename = "";
 
   //-- Sacamos la ruta (pathname)
   //-- Comprobamos si la rut es elemento raiz
   //-- Obtener fichero a devolver
   if (myURL.pathname == "/"){
-    filename += "/tienda.html"  //-- Abrimos la pagina principal
+    filename += "tienda.html";  //-- Abrimos la pagina principal
   }else{
-    filename += myURL.pathname  //-- Abrimos la ruta solicitada
+    filename += myURL.pathname.substr(1);  //-- Abrimos la ruta solicitada
   }
 
   //-- Ruta asignada
-  console.log('Ruta: ' + filename)
- 
+  console.log('Ruta: ' + filename);
+  
   //-- Extraigo el tipo de mime que es la ruta
   //-- me quedo con la extenson
-  let ext = filename.split(".")[1]
+  let ext = filename.split(".")[1];
 
   //-- Escribimos el tipo de mime pedido
-  console.log('Tipo de dato pedido: ' + ext)
+  console.log('Tipo de dato pedido: ' + ext);
 
   //-- Definimos los tipos de mime
   const mimeType = {
@@ -56,23 +56,23 @@ const server = http.createServer(function (req, res) {
     "png"  : "image/png",
     "gif"  : "image/gif",
     "ico"  : "image/x-icon"
-  }
+  };
 
   //-- Asignamos que tipo de mime leer
-  let mime = mimeType[ext]
-  console.log("mime: " + mime)
+  let mime = mimeType[ext];
+  console.log("mime: " + mime);
 
   fs.readFile(filename, function(err, data){
     //-- Controlar si la pagina es no encontrada.
     //-- Devolvemos nuesta pagina de error, 404 NOT FOUND
     if (err){
       res.writeHead(404, {'Content-Type': mime});
-      console.log("Not found")
+      console.log("Not found");
     }else{
       //-- Todo correcto
       //-- Manda el mensaje 200 OK
       res.writeHead(200, {'Content-Type': mime});
-      console.log("Peticion Atendida, 200 OK")
+      console.log("Peticion Atendida, 200 OK");
     } 
     //-- Enviamos los datos del fichero solicitado  
     res.write(data);
