3332792d0122ea73e9a990008fe8fa83dc908450
anapgh <anapogarciah@gmail.com>
2021-03-16 16:41:33 +0100

    Ejercicio3: modificacion json

--- P2/L5/tienda-modi.js
+++ P2/L5/tienda-modi.js
@@ -19,3 +19,34 @@ const tienda_json = fs.readFileSync(FICHERO_JSON);
 
 //-- Crear la estructura tienda a partir del contenido del fichero
 const tienda = JSON.parse(tienda_json);
+
+//-- Imprimir contenido
+console.log("contenido del fichero")
+console.log(tienda)
+
+//-- Incrementamos el numero de todos los productos en 1
+//-- Cambiamos los productos (indice 0)
+productos = tienda[0]["productos"] //-- Nos quedamos con los productos
+console.log("productos de la tienda");
+console.log(productos);
+
+//-- Defino numero de productos
+const N = 4;
+//-- Me quedo con cada stock de cada producto (indice 3)
+//-- y se lo incrementamos en 1
+for(i = 0; i <N; i++){
+    productos[i]["stock"] += 1;
+}
+
+console.log("productos incrementados");
+console.log(productos);
+
+//-- Convertir la variable a cadena JSON
+//-- Convertimos una cadena a JSON con la funcion .stringify
+let myJSON = JSON.stringify(tienda, null, 4);
+
+//-- Guardarla en el fichero destino
+fs.writeFileSync(FICHERO_JSON_OUT, myJSON);
+
+console.log("Información guardada en el fichero: " + FICHERO_JSON_OUT);
+
