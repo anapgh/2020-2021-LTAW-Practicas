394d61f2c0e7a1fb3127678c27c3c77e63aa8c73
anapgh <anapogarciah@gmail.com>
2021-03-22 11:54:04 +0100

    Ejemplo2: envio con POST

---
+++ P2/S6/Ejemplo 2/Ej02.js
@@ -0,0 +1,56 @@
+//-- Imprimir información sobre la solicitud recibida
+
+const http = require('http');
+const fs = require('fs');
+const PUERTO = 8081;
+
+//-- Cargar pagina web del formulario
+const FORMULARIO = fs.readFileSync('form2.html','utf-8');
+
+//-- HTML de la página de respuesta
+const RESPUESTA = fs.readFileSync('form2-resp.html', 'utf-8');
+
+//-- SERVIDOR: Bucle principal de atención a clientes
+const server = http.createServer((req, res) => {
+
+  //-- Construir el objeto url con la url de la solicitud
+  //-- De aqui leo los metodo de la URL
+  const myURL = new URL(req.url, 'http://' + req.headers['host']);  
+  console.log("");
+  console.log("Método: " + req.method); //-- metodo
+  console.log("Recurso: " + req.url); //-- recurso
+  console.log("  Ruta: " + myURL.pathname); //-- ruta sin parametros
+  console.log("  Parametros: " + myURL.searchParams); //-- parametos separados
+
+  //-- Por defecto entregar formulario
+  let content_type = "text/html"; //-- le digo de que tipo es
+  let content = FORMULARIO; //-- aqui por defecto siempre devolvemos el formulario
+
+  //-- En el caso de ir /procesar mandar la respuesta
+  if (myURL.pathname == '/procesar') {
+      content_type = "text/html";
+      content = RESPUESTA;
+  }
+
+  //-- Esto es un stream de flujo de datos
+  //-- Si hay datos en el cuerpo, se imprimen
+  req.on('data', (cuerpo) => {
+
+    //-- Los datos del cuerpo son caracteres
+    req.setEncoding('utf8');
+    console.log(`Cuerpo (${cuerpo.length} bytes)`)
+    console.log(` ${cuerpo}`);
+  });
+
+  //-- Esto solo se ejecuta cuando llega el final del mensaje de solicitud
+  req.on('end', ()=> {
+    //-- Generar respuesta
+    res.setHeader('Content-Type', content_type);
+    res.write(content);
+    res.end()
+  });
+
+});
+
+server.listen(PUERTO);
+console.log("Escuchando en puerto: " + PUERTO);
