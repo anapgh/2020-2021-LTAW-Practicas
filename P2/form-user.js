c3adf1fe8155509731ccfa656a6718f78640e522
anapgh <anapogarciah@gmail.com>
2021-04-06 14:05:04 +0200

    detectado el usuario registrado, bienve y error

--- P2/form-user.js
+++ P2/form-user.js
@@ -7,11 +7,34 @@ const PUERTO = 8080;
 //-- Cargar pagina web del formulario
 const FORMULARIO = fs.readFileSync('form-user.html','utf-8');
 
+//-- Nombre del fichero JSON a leer
+const FICHERO_JSON = "tienda.json"
+
+//-- Leer el fichero JSON
+//-- de esta forma lo hacemos sincrona
+const  tienda_json = fs.readFileSync(FICHERO_JSON);
+
 //-- HTML de la página de respuesta  de Bienvenido
 const RESPUESTAOK = fs.readFileSync('form-user-res.html', 'utf-8');
 
 //-- HTML de la pagina de respuesta de error, cliente desconocido
 const RESPUESTAERROR = fs.readFileSync('form-user-res-error.html', 'utf-8');
+
+//-- Crear la estructura tienda a partir del contenido del fichero
+//-- nos devuelve la estructura del json
+const tienda = JSON.parse(tienda_json);
+
+//-- Defino arrays
+let nombre_reg = []
+let apellidos_reg = []
+
+//-- Imprimir usuarios  registrados
+//-- Recorrer el json para buscar los clientes registrados
+let usuarios_reg = tienda[1]["usuarios"]
+for (i = 0; i < usuarios_reg.length; i++){
+    nombre_reg.push(usuarios_reg[i]["nombre"])
+    apellidos_reg.push(usuarios_reg[i]["apellidos"])
+};
 
 //-- SERVIDOR: Bucle principal de atención a clientes
 const server = http.createServer((req, res) => {
@@ -25,14 +48,25 @@ const server = http.createServer((req, res) => {
   console.log("  Ruta: " + myURL.pathname); //-- ruta sin parametros
   console.log("  Parametros: " + myURL.searchParams); //-- parametos separados
 
+  //-- Leer los parámetros
+  let nombre = myURL.searchParams.get('nombre');
+  let apellidos = myURL.searchParams.get('apellidos');
+  console.log(" Nombre: " + nombre);
+  console.log(" Apellidos: " + apellidos);
+
   //-- Por defecto entregar formulario
   let content_type = "text/html"; //-- le digo de que tipo es
   let content = FORMULARIO; //-- aqui por defecto siempre devolvemos el formulario
 
   //-- En el caso de ir /procesar mandar la respuesta
   if (myURL.pathname == '/procesar') {
-      content_type = "text/html";
-      content = RESPUESTAOK;
+        content_type = "text/html";
+        if ((nombre_reg.includes(nombre)) && (apellidos_reg.includes(apellidos))) {
+            console.log('Usuario registrado');
+            content = RESPUESTAOK;
+        }else{
+            content = RESPUESTAERROR;
+        }
   }
 
   //-- Esto es un stream de flujo de datos
