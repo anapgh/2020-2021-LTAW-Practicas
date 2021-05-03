d6f2ae115962a80104d763a542a3f6dbebf87e28
anapgh <anapogarciah@gmail.com>
2021-05-03 21:20:32 +0200

    Ejemplo5: Servidor web + websockets con express

--- P3/S9/05-Websocket-API-server.js
+++ P3/S9/05-Websocket-API-server.js
@@ -1,10 +1,15 @@
 //-- Importamos modulos externos
+//-- Modulo wesocket
 const WebSocketServer = require('websocket').server;
+//-- Modulo http
 const http = require('http');
+//-- Modulo express
 const express = require('express');
+//-- Modulo colors
 const colors = require('colors');
 
-const PUERTO = 8080;
+//-- Establecemos un puerto
+const PUERTO = 8081;
 
 //-- Crear una aplicación web vacia con express
 const app = express();
@@ -23,6 +28,7 @@ wsServer.on('request', (req) => {
     const connection = req.accept();
 
     //-- Retrollamada de mensaje recibido
+    //-- El mensaje se encuentra en el objeto 'message'
     connection.on('message', (message) => {
         console.log("MENSAGE RECIBIDO");
         console.log("  Tipo de mensaje: " + message.type);
@@ -51,7 +57,7 @@ app.get('/', function (req, res) {
 //-- El directorio publico contiene ficheros estáticos
 app.use(express.static('public'));
 
-//-- Lanzar el servidor HTTP
+//-- Lanzar el servidor HTTP --> ¡COMO SIEMPRE!
 //-- ¡Que empiecen los juegos de los WebSockets!
 server.listen(PUERTO);
 console.log("Escuchando en puerto: " + PUERTO);
