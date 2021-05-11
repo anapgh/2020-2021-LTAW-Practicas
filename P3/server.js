7659940e1d481a99711b5009c6bc7a4551e8ede8
anapgh <anapogarciah@gmail.com>
2021-05-11 13:17:56 +0200

    lo basico estaria hecho, falta css

--- P3/server.js
+++ P3/server.js
@@ -46,7 +46,9 @@ const io = socket(server);
 //-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
 //-- Definir el punto de entrada principal de mi aplicación web
 app.get('/', (req, res) => {
-  res.send('Bienvenido al chat!!!' + '<p><a href="/index.html">Unirse</a></p>');
+  path = __dirname + '/public/index.html';
+  res.sendFile(path);
+  console.log("Solicitado acceso al chat");
 });
 
 //-- Esto es necesario para que el servidor le envíe al cliente la
