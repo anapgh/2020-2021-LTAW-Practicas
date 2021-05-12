4c09089d81ccb923f0c1c8567d53fea0e474cb21
anapgh <anapogarciah@gmail.com>
2021-05-12 10:12:06 +0200

    Version2: Modulo app. Añadiendo el evento ready

--- P4/S10/main.js
+++ P4/S10/main.js
@@ -1,5 +1,13 @@
 //-- Cargar el módulo de electron
-const electron = require('./node_modules/electron');
+const electron = require('electron');
 
 //-- El control lo tiene electron
 console.log("Arrancando electron...");
+
+//-- Punto de entrada. En cuanto electron está listo,
+//-- ejecuta esta función
+//-- El evento ready nos indica cuando los modulos de electron estan cargados
+//-- y asi ejecutar el proceso principal
+electron.app.on('ready', ()=>{
+    console.log("Evento Ready!")
+});
