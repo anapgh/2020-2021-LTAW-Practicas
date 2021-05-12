d15d9b893f5eab1f5a3218f12d0853939dbc6adf
anapgh <anapogarciah@gmail.com>
2021-05-12 10:34:19 +0200

    Version8: Proceso renderizado asociado a interfaz

---
+++ P4/S10/index.js
@@ -0,0 +1,11 @@
+//-- Es el proceso de renderizado
+console.log("Hola desde el proceso de la web...");
+
+//-- Obtener elementos de la interfaz
+const btn_test = document.getElementById("btn_test");
+const display = document.getElementById("display");
+
+btn_test.onclick = () => {
+    display.innerHTML += "TEST! ";
+    console.log("Botón apretado!");
+}
